0c267e907ae86c8524c3fb393c29ea1ef96121a4
Venoninho23 <mauriciolucas416@gmail.com>
2024-05-28 21:29:56 -0300

    agenda_completa

---
+++ tests/agenda.test.ts
@@ -0,0 +1,33 @@
+// tests/agenda.test.ts
+
+import { Agenda } from '../src/agenda';
+
+describe('Teste da Agenda', () => {
+    let agenda: Agenda;
+
+    beforeEach(() => {
+        agenda = new Agenda();
+    });
+
+    test('deve adicionar um compromisso corretamente', () => {
+        agenda.adicionarCompromisso('2024-05-27', '14:00', 'Reunião de trabalho');
+        expect(agenda.listarCompromissos()).toEqual([
+            { data: '2024-05-27', hora: '14:00', descricao: 'Reunião de trabalho' }
+        ]);
+    });
+
+    test('deve remover um compromisso corretamente', () => {
+        agenda.adicionarCompromisso('2024-05-27', '14:00', 'Reunião de trabalho');
+        agenda.removerCompromisso('2024-05-27', '14:00');
+        expect(agenda.listarCompromissos()).toEqual([]);
+    });
+
+    test('deve listar todos os compromissos corretamente', () => {
+        agenda.adicionarCompromisso('2024-05-27', '14:00', 'Reunião de trabalho');
+        agenda.adicionarCompromisso('2024-05-28', '10:00', 'Consulta médica');
+        expect(agenda.listarCompromissos()).toEqual([
+            { data: '2024-05-27', hora: '14:00', descricao: 'Reunião de trabalho' },
+            { data: '2024-05-28', hora: '10:00', descricao: 'Consulta médica' }
+        ]);
+    });
+});
